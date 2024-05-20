939618b112638d8532e8103e024ebc1a2e05d92f
ruizhipu <18651885620@163.com>
2024-05-21 03:19:24 +0800

    debug

--- DAR-RSD/MPI3D/source_model.sh
+++ DAR-RSD/MPI3D/source_model.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 python train_source.py --gpu_id 0 --src rc --tgt rl --tradeoff 0.001 --tradeoff2 0.03 
-#python train_source.py --gpu_id 0 --src rl --tgt rc --tradeoff 0.001 --tradeoff2 0.03 
-#python train_source.py --gpu_id 0 --src rl --tgt t --tradeoff 0.001 --tradeoff2 0.03 
-#python train_source.py --gpu_id 0 --src rc --tgt t --tradeoff 0.003 --tradeoff2 0.03 
-#python train_source.py --gpu_id 0 --src t --tgt rc --tradeoff 0.003 --tradeoff2 0.03 
-#python train_source.py --gpu_id 0 --src t --tgt rl --tradeoff 0.003 --tradeoff2 0.03 
+python train_source.py --gpu_id 0 --src rl --tgt rc --tradeoff 0.001 --tradeoff2 0.03 
+python train_source.py --gpu_id 0 --src rl --tgt t --tradeoff 0.001 --tradeoff2 0.03 
+python train_source.py --gpu_id 0 --src rc --tgt t --tradeoff 0.003 --tradeoff2 0.03 
+python train_source.py --gpu_id 0 --src t --tgt rc --tradeoff 0.003 --tradeoff2 0.03 
+python train_source.py --gpu_id 0 --src t --tgt rl --tradeoff 0.003 --tradeoff2 0.03 
