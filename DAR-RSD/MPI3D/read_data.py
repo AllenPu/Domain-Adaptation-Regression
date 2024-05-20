6983f2ec0532dc92cf321d9e318bde70e16815ca
ruizhipu <18651885620@163.com>
2024-05-21 02:59:58 +0800

    debug

--- DAR-RSD/MPI3D/read_data.py
+++ DAR-RSD/MPI3D/read_data.py
@@ -195,6 +195,8 @@ class ImageList(object):
         image_index = int(''.join(re.findall('[0-9]', path)))
         #print(f' key is {image_index}')
         img = self.img[image_index]
+        #
+        img.transpose(1,2,0)
         #if img.shape != (64,64,3):
         #    print(f' index is {index} key is {image_index} shape of the  numpy  {img.shape}')
         img = Image.fromarray(np.uint8(img))
