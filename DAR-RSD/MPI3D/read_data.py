130f17fd93bb1bb080ed048383de2d957d8fde62
ruizhipu <18651885620@163.com>
2024-05-23 15:53:54 +0800

    de bug

--- DAR-RSD/MPI3D/read_data.py
+++ DAR-RSD/MPI3D/read_data.py
@@ -135,12 +135,12 @@ def accimage_loader(path):
         return pil_loader(path)
 
 
-def default_loader(path, npz_file=None):
+def default_loader(path, img_path=''):
     # from torchvision import get_image_backend
     # if get_image_backend() == 'accimage':
     #    return accimage_loader(path)
     # else:
-    return pil_loader(path)
+    return pil_loader(path, img_path)
 
 
 
